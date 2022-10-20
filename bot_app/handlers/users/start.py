04233e4277b6f096cda414083e7f6af33dee78b2
liskunovich <david.leonov.96@mail.ru>
2022-10-20 12:25:54 -0400

    Made new logic for db filling

--- bot_app/handlers/users/start.py
+++ bot_app/handlers/users/start.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from bot_app.keyboards.default import start, menu
@@ -14,23 +16,19 @@ def write_to_db(*args):
 
 @sync_to_async
 def is_exists(telegram_id: str):
-    return Client.objects.filter(telegram_id=telegram_id).exists()
+    return Client.objects.filter(telegram_id=telegram_id, number__isnull=False).exists()
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!',
-                         reply_markup=start.PHONE_REQUEST_KEYBOARD if not (
-                             await is_exists(message.from_user.id)) else menu.MAIN_MENU_KEYBOARD)
-    user_id = message.from_user.id
-    username = message.from_user.username
-    await write_to_db(user_id, username, None)
+    await message.answer(f'Здравствуйте, {message.from_user.full_name}!',
+                         reply_markup=menu.MAIN_MENU_KEYBOARD if (
+                             await is_exists(message.from_user.id)) else start.PHONE_REQUEST_KEYBOARD)
 
 
 @dp.message_handler(content_types=types.ContentType.CONTACT)
 async def get_number(message: types.Contact):
     message_data = message.contact
-    if await is_exists(message_data["user_id"]):
-        await message.answer("Вы перешли в главное меню", reply_markup=menu.MAIN_MENU_KEYBOARD)
-    else:
-        await write_to_db(message_data["user_id"], message_data["first_name"], message_data["phone_number"])
+    await asyncio.gather(
+         write_to_db(message_data["user_id"], message_data["first_name"], message_data["phone_number"]),
+         message.answer("Вы перешли в главное меню", reply_markup=menu.MAIN_MENU_KEYBOARD))
